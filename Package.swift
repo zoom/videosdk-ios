6ff54759613995b9f3b000e27410131fc8d161fe
Elaine Ku <elaine.ku@zoom.us>
2025-10-14 17:36:42 -0700

    v2.3.10

--- Package.swift
+++ Package.swift
@@ -3,35 +3,29 @@
 
 import PackageDescription
 
+let url = "https://github.com/zoom/zoom-video-sdk-iOS/releases/download/v2.3.10/zoom-video-sdk-iOS.zip"
+let checksum = "7690696486afb67de0cc096414634b2b2647c0a0966411a0910437b63ec32f37"
+
 let package = Package(
-    name: "ZoomVideoSDK-iOS",
-    platforms: [
-            .iOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ZoomVideoSDK-iOS",
-            targets: ["ZoomVideoSDK", "CptShare", "zoomcml", "zm_annoter_dynamic"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-                    name: "ZoomVideoSDK",
-                    path: "ZoomVideoSDK.xcframework"),
-        .binaryTarget(
-                    name: "CptShare",
-                    path: "CptShare.xcframework"),
-        .binaryTarget(
-                    name: "zoomcml",
-                    path: "zoomcml.xcframework"),
-        .binaryTarget(
-                    name: "zm_annoter_dynamic",
-                    path: "zm_annoter_dynamic.xcframework"),
-    ]
+  name: "ZoomVideoSDK-iOS",
+  platforms: [.iOS(.v15)],
+  products: [
+    .library(name: "ZoomVideoSDK",                targets: ["ZoomVideoSDK"]),
+    .library(name: "CptShare",                    targets: ["CptShare"]),
+    .library(name: "zoomcml",                     targets: ["zoomcml"]),
+    .library(name: "zm_annoter_dynamic",          targets: ["zm_annoter_dynamic"]),
+    .library(name: "ZoomTask",                    targets: ["ZoomTask"]),
+    .library(name: "Whiteboard",                  targets: ["Whiteboard"]),
+    .library(name: "ZoomVideoSDKScreenShare",     targets: ["ZoomVideoSDKScreenShare"]),
+  ],
+  targets: [
+    .binaryTarget(name: "ZoomVideoSDK",            url: url, checksum: checksum),
+    .binaryTarget(name: "CptShare",                url: url, checksum: checksum),
+    .binaryTarget(name: "zoomcml",                 url: url, checksum: checksum),
+    .binaryTarget(name: "zm_annoter_dynamic",      url: url, checksum: checksum),
+    .binaryTarget(name: "ZoomTask",                url: url, checksum: checksum),
+    .binaryTarget(name: "Whiteboard",              url: url, checksum: checksum),
+    .binaryTarget(name: "ZoomVideoSDKScreenShare", url: url, checksum: checksum),
+  ]
 )
+
